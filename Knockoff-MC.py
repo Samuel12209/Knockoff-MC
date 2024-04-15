fe9ae1e59678fbd98d450572ecf4dbe526f6efea
Samuel12209 <samuelbenitez12290@gmail.com>
2024-04-15 17:27:45 -0400

    basic template

--- Knockoff-MC.py
+++ Knockoff-MC.py
@@ -1,20 +1,33 @@
+
 from ursina import *
+from ursina.prefabs.first_person_controller import *
+
 
 app = Ursina()
 
-gravity = 0.1
-jump_height = 0.2
+class Voxel(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.5,
+            texture='white_cube',
+            color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+            highlight_color=color.lime,
+        )
 
-player = Entity(
-    model = 'cube' ,
-    color = color.green,
-    scale_y = 2
-    )
+for z in range(8):
+    for x in range(8):
+        voxel = Voxel(position=(x,0,z))
 
-def update():
-    player.x += held_keys['d'] * .1
-    player.x -= held_keys['a'] * .1
-    player.y += held_keys['w'] * .1
-    player.y -= held_keys['s'] * .1
 
+def input(key):
+    if key == 'right mouse down':
+        hit_info = raycast(camera.world_position, camera.forward, distance=5)
+        if hit_info.hit:
+            Voxel(position=hit_info.entity.position + hit_info.normal)
+    if key == 'left mouse down' and mouse.hovered_entity:
+        destroy(mouse.hovered_entity)
+
+player = FirstPersonController()
 app.run()
