56bd0ccf995189caf2511317df84ffd2e218385e
Samuel12209 <samuelbenitez12290@gmail.com>
2024-04-19 19:27:34 -0400

    Flat world generated

--- Knockoff-MC.py
+++ Knockoff-MC.py
@@ -1,7 +1,8 @@
 
 from ursina import *
 from ursina.prefabs.first_person_controller import *
-
+import random
+import math
 
 app = Ursina()
 
@@ -15,6 +16,23 @@ class Voxel(Button):
             color=color.hsv(0, 0, random.uniform(.9, 1.0)),
             highlight_color=color.lime,
         )
+for i in range(1):
+  angle = i
+  x = 20 * math.cos(math.radians(angle))
+
+for x in range(50):
+    for y in range(1):
+        for z in range(50):
+            voxel = Voxel(position=(x, y, z))
+
+for angle in range(1):
+    x = 100 * math.cos(math.radians(angle)) 
+    y = 0
+    z = 100 * math.sin(math.radians(angle))
+    voxel = Voxel(position=(x, y, z))
+    voxel = Voxel(position=(x, 0, z))
+
+
 
 for z in range(8):
     for x in range(8):
