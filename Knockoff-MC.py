e7d8f529110f9f8e1329baebffdf0d8c3b5d79dc
Samuel12209 <samuelbenitez12290@gmail.com>
2024-04-20 16:01:10 -0400

    added trees

--- Knockoff-MC.py
+++ Knockoff-MC.py
@@ -1,14 +1,14 @@
-
 from ursina import *
 from ursina.prefabs.first_person_controller import *
 import random
 import math
 
 app = Ursina()
+current_delay = 0.9
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
-        super().__init__(parent=scene,
+        super().__init__(parent=scene, 
             position=position,
             model='cube',
             origin_y=.5,
@@ -17,37 +17,68 @@ class Voxel(Button):
             highlight_color=color.lime,
         )
 
-for i in range(1):
-  angle = i
-  x = 20 * math.cos(math.radians(angle))
-
+voxels = []
 for x in range(50):
     for y in range(1):
         for z in range(50):
             voxel = Voxel(position=(x, y, z))
-
-
-for angle in range(1):
-    x = 100 * math.cos(math.radians(angle)) 
-    y = 0
-    z = 100 * math.sin(math.radians(angle))
-    voxel = Voxel(position=(x, y, z))
-    voxel = Voxel(position=(x, 0, z))
-
-
+            voxels.append(voxel)
 
 for z in range(8):
     for x in range(8):
         voxel = Voxel(position=(x,0,z))
+        voxels.append(voxel)
+
+tree_num = random.randrange(1, 2)
+for i in range(50):
+    if tree_num == 1:
+        class Wood(Button):
+            def __init__(self, position=(0,0,0)):
+                super().__init__(parent=scene, 
+                position=position,
+                model='cube',
+                origin_y=.5,
+                texture='wood.png',
+                color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+                highlight_color=color.lime,
+            )
+
+        tree_x = random.randint(0, 49)
+        tree_z = random.randint(0, 49)
+        voxel = Wood(position=(tree_x, 1, tree_z))
+        voxel = Wood(position=(tree_x, 2, tree_z))
+        voxel = Wood(position=(tree_x, 3, tree_z))
+        voxel = Wood(position=(tree_x, 4, tree_z))
+        voxel = Wood(position=(tree_x, 5, tree_z))
+        #leaves
+        class Leaf(Button):
+            def __init__(self, position=(0,0,0)):
+                super().__init__(parent=scene, 
+                position=position,
+                model='cube',
+                origin_y=.5,
+                texture='leaf.png',
+                color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+                highlight_color=color.lime,
+            )
+
+        voxel = Leaf(position=(tree_x, 6, tree_z))
+        voxel = Leaf(position=(tree_x + 1, 5, tree_z))
+        voxel = Leaf(position=(tree_x - 1, 5, tree_z))
+        voxel = Leaf(position=(tree_x, 5, tree_z + 1))
+        voxel = Leaf(position=(tree_x, 5, tree_z - 1))
+           
 
 
 def input(key):
     if key == 'right mouse down':
         hit_info = raycast(camera.world_position, camera.forward, distance=5)
         if hit_info.hit:
-            Voxel(position=hit_info.entity.position + hit_info.normal)
+            voxel = Voxel(position=hit_info.entity.position + hit_info.normal)
+            voxels.append(voxel)
+            
     if key == 'left mouse down' and mouse.hovered_entity:
-        destroy(mouse.hovered_entity)
+        destroy(mouse.hovered_entity, delay=current_delay)
 
 player = FirstPersonController()
 app.run()
